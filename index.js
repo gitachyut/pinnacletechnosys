d91e77d488ba18c59fecd65a91878591d38e37f0
Achyut Krishna Deka <you@example.com>
2017-08-30 19:59:40 +0530

    Demo

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const express = require('express')
-const app = express()
+var cool = require('cool-ascii-faces');
+var express = require('express');
+var app = express();
 
-app.get('*',(req,res)=>{
-  res.send('helllo')
-})
+app.set('port', (process.env.PORT || 5000));
 
+app.use(express.static(__dirname + '/public'));
 
-app.listen(5000,()=>{
-  console.log('app runing on port 5000');
-})
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index')
+});
+
+app.get('/cool', function(request, response) {
+  response.send(cool());
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
